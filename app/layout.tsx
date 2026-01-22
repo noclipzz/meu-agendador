23d526ccdee846ed45d8c56bf2f4def8160d4606
Yan Kairon <yan.kairon@gmail.com>
2026-01-21 21:26:33 -0300

    Corrigindo versao do Clerk e adicionando login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; // <--- Essa linha é a que faz a mágica do visual!
+import { ClerkProvider } from '@clerk/nextjs' // <--- 1. Importar isso
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
+    // 2. Envolver tudo com o ClerkProvider
+    <ClerkProvider>
+      <html lang="pt-BR">
+        <body className={inter.className}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
