11189da15d2dc7f7930d56eb9f4c642501037c1a
Yan Kairon <yan.kairon@gmail.com>
2026-01-23 16:42:53 -0300

    Arrumando nomes finais das rotas

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Rotas que PRECISAM de senha
 const isProtectedRoute = createRouteMatcher([
   '/admin(.*)',
+  '/meus-agendamentos(.*)'
 ]);
 
 export default clerkMiddleware((auth, req) => {
+  // Se for rota protegida, bloqueia. Se não for, deixa passar.
   if (isProtectedRoute(req)) {
     auth().protect();
   }
