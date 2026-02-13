1be289dcda4bc5f664762d8fed2d41829539f0ba
Yan Kairon <yan.kairon@gmail.com>
2026-02-13 13:00:48 -0300

    fix: update corrupted VAPID keys

--- lib/push-notifications.ts
+++ lib/push-notifications.ts
@@ -1,6 +1,6 @@
 "use client";
 
-const VAPID_PUBLIC_KEY = "BFWkYyZs30s3mcN-A777dG1uv9Zjgs7n7DFHDqezCUkhRDrliHNbUkRyBw5_her_sV5QGtUCxKV9LcaNALxo9R0";
+const VAPID_PUBLIC_KEY = "BG6vrlAV2S7nXPuXMK_tZ2X5yvVb1FVPxoGKvgL65ab6t-CGIVxSyeQt6DgxL3d4-tF8G2yvJz_hO6a6Q6p6jU8";
 
 function urlBase64ToUint8Array(base64String: string) {
     const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
