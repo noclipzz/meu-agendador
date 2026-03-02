47a86de2211df063ff1532b6f05ebbc82ebc847f
Yan Kairon <yan.kairon@gmail.com>
2026-03-02 15:19:59 -0300

    fix(sigcorp): updated addReference from positional arguments to object expected by xml-crypto v6 API resolving digestAlgorithm signature error

--- lib/nfe/sigcorp.ts
+++ lib/nfe/sigcorp.ts
@@ -58,7 +58,13 @@ function signXML(xml: string, tagToSign: string, pfxBase64OrBuffer: any, passwor
 
         // No RPS Padrão ABRASF, geralmente assinamos a Tag do <InfDeclaracaoPrestacaoServico> ou <Rps> se for RPS simples
         // @ts-ignore
-        sig.addReference(`//*[local-name(.)='${tagToSign}']`, ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"], "http://www.w3.org/2000/09/xmldsig#sha1", "", "", "", true);
+        sig.addReference({
+            xpath: `//*[local-name(.)='${tagToSign}']`,
+            transforms: ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"],
+            digestAlgorithm: "http://www.w3.org/2000/09/xmldsig#sha1",
+            uri: "",
+            isEmptyUri: true
+        });
 
         // @ts-ignore
         sig.signingKey = pemKey;
