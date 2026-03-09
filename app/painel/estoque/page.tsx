fe610e858d27478054809e19125b897da16c7c7f
Yan Kairon <yan.kairon@gmail.com>
2026-03-09 18:32:24 -0300

    style: widen product details modal for better visualization

--- app/painel/estoque/page.tsx
+++ app/painel/estoque/page.tsx
@@ -231,7 +231,7 @@ export default function EstoquePage() {
             {/* MODAL GLOBAL */}
             {modalOpen && (
                 <ModalPortal><div className="fixed inset-0 bg-black/80 backdrop-blur-sm flex justify-center items-center z-[110] p-4">
-                    <div className="bg-white dark:bg-gray-900 w-full max-w-2xl rounded-[3rem] shadow-2xl relative overflow-hidden flex flex-col max-h-[90vh]">
+                    <div className="bg-white dark:bg-gray-900 w-full max-w-4xl rounded-[3rem] shadow-2xl relative overflow-hidden flex flex-col max-h-[95vh]">
 
                         {/* HEADER DO MODAL */}
                         <div className="p-8 pb-4 border-b dark:border-gray-800 bg-gray-50/50 dark:bg-white/5">
